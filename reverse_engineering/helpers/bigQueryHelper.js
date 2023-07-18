57dd0d7a45c802024fdd03c3c3a9af6936039557
Danylo Strelets <danylo.strelets@hackolade.com>
2023-07-18 17:19:41 +0300

    HCK-3553: remove unnecessary statement

--- reverse_engineering/helpers/bigQueryHelper.js
+++ reverse_engineering/helpers/bigQueryHelper.js
@@ -82,10 +82,6 @@ const createBigQueryHelper = (client, log) => {
 	};
 
 	const getSampleDocSize = (count, recordSamplingSettings) => {
-		if (recordSamplingSettings.active === 'absolute') {
-			return Number(recordSamplingSettings.absolute.value);
-		}
-
 		const limit = Math.ceil((count * recordSamplingSettings.relative.value) / 100);
 
 		return Math.min(limit, recordSamplingSettings.maxValue);
