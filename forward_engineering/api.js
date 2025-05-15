b35ac955c23f5eda9c039e954c505f600ae20318
Yevhenii Moroziuk <yevhen.moroziuk@hackolade.com>
2025-05-15 13:03:42 +0300

    HCK-11091: Move the log of the start to the studio (#71)

--- forward_engineering/api.js
+++ forward_engineering/api.js
@@ -19,9 +19,6 @@ module.exports = {
 	},
 
 	applyToInstance(connectionInfo, logger, callback, app) {
-		logger.clear();
-		logger.log('info', connectionInfo, 'connectionInfo', connectionInfo.hiddenKeys);
-
 		applyToInstanceHelper
 			.applyToInstance(connectionInfo, logger, app)
 			.then(result => {
