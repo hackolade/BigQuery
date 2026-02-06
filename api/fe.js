e1d0e69db8232255f2f2e665acd6475ed9971476
Alik Rakhmonov <alik.rahmonov@gmail.com>
2026-02-06 10:29:10 +0100

    fix: unresolved imports

--- api/fe.js
+++ api/fe.js
@@ -1,7 +1,7 @@
-const { generateScript } = require('../forward_engineering/generateScript');
-const { generateContainerScript } = require('../forward_engineering/generateContainerScript');
-const { generateViewScript } = require('../forward_engineering/generateViewScript');
-const { isDropInStatements } = require('../forward_engineering/isDropInStatements');
+const { generateScript } = require('../forward_engineering/api/generateScript');
+const { generateContainerScript } = require('../forward_engineering/api/generateContainerScript');
+const { generateViewScript } = require('../forward_engineering/api/generateViewScript');
+const { isDropInStatements } = require('../forward_engineering/api/isDropInStatements');
 
 module.exports = {
 	generateScript,
